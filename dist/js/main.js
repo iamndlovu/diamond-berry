932e339757992c7c8525e1f3c385b063058e0dcc
iamndlovu <ndlovu.p@protonmail.com>
2020-07-14 21:59:04 +0200

    slide show in home page

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+function toggleSlides() {
+  let next = document.querySelector('.slide.next'),
+      prev = document.querySelector('.slide.prev'),
+      showing = document.querySelector('.slide.show');
+    next.classList.remove('next');
+    next.classList.add('show');
+    showing.classList.remove('show');
+    showing.classList.add('prev');
+    prev.classList.remove('prev');
+    prev.classList.add('next');
+}
+
+let toggle = setInterval(toggleSlides, 3000);
